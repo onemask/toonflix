e254b69584cc71fcdba21055304b05e3c5f4be1b
Soo_ <onemask@naver.com>
2023-04-18 00:13:25 +0900

    first add

---
+++ lib/models/webtoon_model.dart
@@ -0,0 +1,8 @@
+class WebtoonModel {
+  final String title, thumb, id;
+
+  WebtoonModel.fromjson(Map<String, dynamic> json)
+      : title = json['title'],
+        thumb = json['title'],
+        id = json['title'];
+}
